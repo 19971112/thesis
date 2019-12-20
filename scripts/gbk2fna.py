13b08c227276cea73d9d960f7f566ef75a9b52bd
19971112 <warashinatomorrow1112@gmail.com>
2019-12-20 14:40:20 +0900

    Create gbk2fna.py

---
+++ scripts/gbk2fna.py
@@ -0,0 +1,6 @@
+import sys
+from Bio import SeqIO
+filename = sys.argv[1] # Takes first command line argument input filename
+
+from Bio import SeqIO
+SeqIO.convert(filename, "genbank", filename+".fna", "fasta")
