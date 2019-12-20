bcedbc08bd185ea277b8f212578b9435868422aa
19971112 <warashinatomorrow1112@gmail.com>
2019-12-20 14:46:48 +0900

    Update 7_ls-bsr.sh

--- scripts/7_ls-bsr.sh
+++ scripts/7_ls-bsr.sh
@@ -3,3 +3,15 @@
 #PBS -l ncpus=1
 #PBS -V
 
+mkdir -p analysis/ls-bsr && cd $_
+
+mkdir {data,db,query}
+
+# make database (fna)
+cp ../../data/dataset/*.gbff ./data
+for f in ./data/*.gbff; do python ../../scripts/gbk2fna.py $f; done
+mv data/*.fasta db
+
+# make query (faa)
+for f in ./data/*.gbff; do python ../../scripts/gbk2faa.py $f; done
+
