86e9705669a637b769e664f26992fbf467eaaa69
19971112 <warashinatomorrow1112@gmail.com>
2019-12-20 12:40:54 +0900

    Update and rename RSUC.sh to 5_RSUC.sh

--- scripts/5_RSUC.sh
+++ scripts/5_RSUC.sh
@@ -5,7 +5,7 @@
 cd ${PBS_O_WORKDIR}
 
 mkdir -p analysis/RSCU && cd $_
-python ../../scripts/list.py "../../data/dataset3/*.gbff" | grep -vi "plasmid" > list.txt
+python ../../scripts/list.py "../../data/dataset/*.gbff" | grep -vi "plasmid" > list.txt
 for i in $(cut -f1 list.txt); do Rscript ../../scripts/RSCU.R $i; done
 
 
