595e3f24fb6fab293bdf5d40ab36d41b123cf65c
19971112 <warashinatomorrow1112@gmail.com>
2020-01-06 19:06:28 +0900

    Update 6_Roary.sh

--- scripts/6_Roary.sh
+++ scripts/6_Roary.sh
@@ -16,4 +16,6 @@ perl ../../../scripts/bp_genbank2gff3.pl ../../../data/dataset/*.gbff
 cd ..
 
 # Roaryの実行
+roary -f ./i90/ -e -p 40 -n -i 90 -g 500000 -v ./dataset/*.gff
+roary -f ./i95/ -e -p 40 -n -i 95 -g 500000 -v ./dataset/*.gff
 # roary -f ./roary_Geobacter_spp_i90/ -e -p 40 -n -i 90 -g 500000 -v ./dataset/*.gff
