506b5704d953e946d1455ebc6ebebdaa480d26ed
19971112 <warashinatomorrow1112@gmail.com>
2020-01-06 19:06:50 +0900

    Update 6_Roary.sh

--- scripts/6_Roary.sh
+++ scripts/6_Roary.sh
@@ -6,7 +6,7 @@ cd ${PBS_O_WORKDIR}
 
 mkdir -p analysis/Roary && cd $_
 
-mkdir -p {gbff,gff}
+mkdir -p {gff}
 
 # データセットのコピー
 # cp ../../data/dataset/*.gbff ./gbff
@@ -16,6 +16,6 @@ perl ../../../scripts/bp_genbank2gff3.pl ../../../data/dataset/*.gbff
 cd ..
 
 # Roaryの実行
-roary -f ./i90/ -e -p 40 -n -i 90 -g 500000 -v ./dataset/*.gff
-roary -f ./i95/ -e -p 40 -n -i 95 -g 500000 -v ./dataset/*.gff
+roary -f ./i90/ -e -p 40 -n -i 90 -g 500000 -v ./gff/*.gff
+roary -f ./i95/ -e -p 40 -n -i 95 -g 500000 -v ./gff/*.gff
 # roary -f ./roary_Geobacter_spp_i90/ -e -p 40 -n -i 90 -g 500000 -v ./dataset/*.gff
